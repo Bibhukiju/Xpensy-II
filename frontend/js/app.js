7a1fae8bcb0c988a8758413c82a309b5dd9266e2
chetan-budathoki <chetanbudhathoki6@gmail.com>
2021-04-06 20:38:56 +0545

    name changed

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,28 +1,36 @@
-//functions
-const data = {
-  email: "bibhukiju11@gmail.com",
-  password: "100000000",
-};
-function submitData() {
-  fetch("http://localhost:3000/signin", {
+
+
+// selectors
+const login = document.querySelector(".btn");
+
+// event listeners
+
+login.addEventListener("click", function () {
+  const email = document.querySelector(".fname").value;
+  const password = document.querySelector(".lname").value;
+  console.log(email);
+  console.log(password);
+
+  fetch("http://xpenc.herokuapp.com/signin", {
     method: "POST",
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+      email,
+      password,
+    }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      "Content-Type": "application/json;charset=UTF-8",
     },
   })
-    .then(function (response) {
+    .then((response) => {
       if (response.ok) {
         return response.json();
       }
       return Promise.reject(response);
     })
-    .then(function (data) {
+    .then((data) => {
       console.log(data);
     })
-    .catch(function (error) {
-      console.warn("Something went wrong.", error);
+    .catch(error=>{
+        console.log(error);
     });
-}
-
-submitData();
+});
