5341b3034fc8988aebbe4e44799c612a910a7041
bibhukiju <bibhukiju@gmail.com>
2021-04-15 21:05:39 +0545

    bugs are fixed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const { MONGOURI } = require("./config/keys");
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGOURI, {
+mongoose.connect(MONGOURI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: true,
